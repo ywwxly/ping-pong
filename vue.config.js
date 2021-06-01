010d3576063b771897e012be898190c72ff2afe4
ywwxly <ywwxly@163.com>
2021-06-01 21:28:40 +0800

    title更改

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  publicPath: './'
+  publicPath: './',
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = '乒乓球视觉训练系统';
+      return args;
+    })
+  },
+
 }
